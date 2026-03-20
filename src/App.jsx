d9a602874a2d6a3096a2e22cc394ff5ec9e29bc2
ELBOUKBIRI MOHAMMED AMINE <amine@ELBOUKBIRIs-MacBook-Pro.local>
2026-03-20 04:47:59 +0000

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="p-6 bg-gray-100 min-h-screen">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Tailwind fonctionne 🚀
+        </h1>
+      </div>
+  )
+}
+
+export default App
